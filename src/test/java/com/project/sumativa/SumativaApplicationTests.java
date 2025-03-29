97835c15c2eec6f25c8aa40fbc09e8690d5d2fbc
Daniel San Juan <dsanjuan1999@gmail.com>
2025-03-29 00:01:50 -0300

    primer commit

---
+++ src/test/java/com/project/sumativa/SumativaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sumativa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumativaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
