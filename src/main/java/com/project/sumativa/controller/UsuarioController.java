8c14467e4ab1dbe85af766880f4ba190fc44899e
Daniel San Juan <dsanjuan1999@gmail.com>
2025-04-21 21:09:33 -0400

    Se implementan logs, se crea carpeta config y sea realiza metodo CRUD

--- src/main/java/com/project/sumativa/controller/UsuarioController.java
+++ src/main/java/com/project/sumativa/controller/UsuarioController.java
@@ -3,15 +3,18 @@ package com.project.sumativa.controller;
 import com.project.sumativa.model.Usuario;
 import com.project.sumativa.service.UsuarioService;
 import com.project.sumativa.model.ResponseWrapper;
-// import com.project.sumativa.exception.UsuarioNotFound;
 
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
-
 import org.springframework.web.bind.annotation.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+
+@Slf4j
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
@@ -22,11 +25,14 @@ public class UsuarioController {
         this.usuarioService = usuarioService;
     }
 
+    // Recupera todos los usuarios
     @GetMapping
     public ResponseEntity<?> getAllUsuarios() {
+        log.debug("GET /usuarios - Recuperando todos los usuarios");
         List<Usuario> usuarios = usuarioService.getAllUsuarios();
 
         if (usuarios.isEmpty()) {
+            log.warn("No hay usuarios registrados");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No hay usuarios registrados.");
         }
 
@@ -38,8 +44,50 @@ public class UsuarioController {
             ));
     }
 
+    // Recupera un usuario por ID
     @GetMapping("/{id}")
     public Usuario getUsuarioById(@PathVariable Long id) {
+        log.debug("GET /usuarios/{} - Recuperando usuario por ID", id);
         return usuarioService.getUsuarioById(id);
+    }
+
+    // Crea un nuevo usuario
+    @PostMapping
+    public ResponseEntity<ResponseWrapper<Usuario>> createUsuario(@RequestBody Usuario usuario) {
+        log.debug("POST /usuarios - Creando nuevo usuario: {}", usuario);
+        Usuario createdUsuario = usuarioService.createUsuario(usuario);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseWrapper<>(
+                    "Usuario creado",
+                    1,
+                    List.of(createdUsuario)
+                ));
+    }
+
+    // Actualiza un usuario existente
+    @PostMapping("/{id}")
+    public ResponseEntity<ResponseWrapper<Usuario>> updateUsuario(@PathVariable Long id, @RequestBody Usuario usuario) {
+        log.debug("POST /usuarios/{} - Actualizando usuario: {}", id);
+        Usuario updatedUsuario = usuarioService.updateUsuario(id, usuario);
+        return ResponseEntity.ok(
+            new ResponseWrapper<>(
+                "Usuario actualizado",
+                1,
+                List.of(updatedUsuario)
+            ));
+    }
+
+    // Elimina un usuario por ID
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUsuario(@PathVariable Long id) {
+        log.debug("DELETE /usuarios/{} - Eliminando usuario", id);
+        usuarioService.deleteUsuario(id);
+        return ResponseEntity.ok(
+            new ResponseWrapper<>(
+                "Usuario eliminado",
+                1,
+                null
+            ));
     }
 }
--- src/main/java/com/project/sumativa/service/UsuarioService.java
+++ src/main/java/com/project/sumativa/service/UsuarioService.java
@@ -9,22 +9,62 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import lombok.extern.slf4j.Slf4j;
 
-
+@Slf4j
 @Service
 public class UsuarioService {
 
     @Autowired
     private UsuarioRepository repo;
     
+    // Obtener todos los usuarios
     public List<Usuario> getAllUsuarios() {
+        log.debug("Servicio: getAllUsuarios()");
         return repo.findAll(Sort.by("id")
             .ascending());
     }
 
+    // Obtener usuario por ID
     public Usuario getUsuarioById(Long id) {
+        log.debug("Servicio: getUsuarioById({})", id);
         return repo.findById(id)
             .orElseThrow(() -> new UsuarioNotFound(id));
     }
 
+    // Crear usuario
+    public Usuario createUsuario(Usuario usuario) {
+        log.debug("Servicio: createUsuario({})", usuario);
+
+        if (repo.existsById(usuario.getId())) {
+            log.error("El usuario con ID {} ya existe", usuario.getId());
+            throw new IllegalArgumentException("El ID de esta usuario ya existe" + usuario.getId());
+        }
+
+        return repo.save(usuario);
+    }
+
+    // Actualizar usuario
+    public Usuario updateUsuario(Long id, Usuario usuarioUpdt) {
+        log.debug("Servicio: updateUsuario({}, {})", id, usuarioUpdt.getNombre());
+
+        Usuario usuarioExiste = repo.findById(id)
+            .orElseThrow(() -> new UsuarioNotFound(id));
+
+        usuarioExiste.setNombre(usuarioUpdt.getNombre());
+        usuarioExiste.setEmail(usuarioUpdt.getEmail());
+        usuarioExiste.setFhaNacimiento(usuarioUpdt.getFhaNacimiento());
+        usuarioExiste.setRol(usuarioUpdt.getRol());
+
+        usuarioUpdt.setId(id);
+        return repo.save(usuarioUpdt);
+    }
+
+    // Eliminar usuario
+    public void deleteUsuario(Long id) {
+        log.debug("Servicio: deleteUsuario({})", id);
+        Usuario usuario = repo.findById(id)
+            .orElseThrow(() -> new UsuarioNotFound(id));
+        repo.delete(usuario);
+    }
 }
